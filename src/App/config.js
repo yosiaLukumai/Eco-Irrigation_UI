831a384368dec7c3bfd4d4d0bfc5edba668b241e
yosiaLukumai <yosialukumai@gmail.com>
2024-06-23 08:02:26 -0700

    port changes

--- src/App/config.js
+++ src/App/config.js
@@ -1,6 +1,6 @@
 const Configuration = {
     development: true,
-    backendUrl: 'http://localhost:3400',
+    backendUrl: 'http://167.71.173.184:3485',
     localStorageKey: 'AFM_tecv1',
     API_keyValue: null,
     ExpirationOfApiKey: null
